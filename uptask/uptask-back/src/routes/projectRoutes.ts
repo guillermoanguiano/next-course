bfabd04eeaa07e24913f04c74b1ffbbde0d731cd
guillermoanguiano <jesus2001guillermo2@gmail.com>
2024-04-02 12:05:43 -0700

    first commit

---
+++ uptask/uptask-back/src/routes/projectRoutes.ts
@@ -0,0 +1,43 @@
+import { Router } from "express";
+import { ProjectController } from "../controllers/ProjectController";
+import { body, param } from "express-validator";
+import { handleInputErrors } from "../middleware/validation";
+
+const router: Router = Router();
+
+router.post(
+  "/",
+  // With this we can validate the body of the request using express-validator
+  body("projectName").notEmpty().withMessage("Project name is required"),
+  body("clientName").notEmpty().withMessage("Client name is required"),
+  body("description").notEmpty().withMessage("Description is required"),
+  // We pass the handleInputErrors middleware to validate the request
+  handleInputErrors,
+  ProjectController.createProject
+);
+
+router.get("/", ProjectController.getAllProjects);
+
+router.get(
+  "/:id",
+  param("id").isMongoId().withMessage("Invalid ID"),
+  handleInputErrors,
+  ProjectController.getProject
+);
+router.put(
+  "/:id",
+  param("id").isMongoId().withMessage("Invalid ID"),
+  body("projectName").notEmpty().withMessage("Project name is required"),
+  body("clientName").notEmpty().withMessage("Client name is required"),
+  body("description").notEmpty().withMessage("Description is required"),
+  handleInputErrors,
+  ProjectController.updateProject
+);
+router.delete(
+  "/:id",
+  param("id").isMongoId().withMessage("Invalid ID"),
+  handleInputErrors,
+  ProjectController.deleteProject
+);
+
+export default router;
